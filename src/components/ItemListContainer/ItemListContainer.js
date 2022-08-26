14c6cdf77d4f2bc19c75def3ba0be00378cd4a08
Lizeth Tancara <liztancarasirpa@gmail.com>
2022-08-26 00:02:34 -0300

    importando el componente ItemCount en ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 import './ItemListContainer.css';
 
 const ItemListContainer = ({greeting}) =>{
+    function onAdd (count) {
+        console.log(`se agregado ${count} productos`)
+    }
+
     return (
         <div className='itemListContainer'>
            <h3 className='titleItemListContainer'>{greeting}</h3>
+            <ItemCount
+                stock={5}
+                initial={1}
+                onAdd={onAdd}
+            />
         </div>
     )
 }
