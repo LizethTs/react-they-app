e2441265a84d7883d87b9f05ae3445e29ff061f5
Lizeth Tancara <liztancarasirpa@gmail.com>
2022-08-31 17:55:48 -0300

    importando el componente ItemList en ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import ItemCount from '../ItemCount/ItemCount';
-import './ItemListContainer.css';
+import React, { useEffect, useState } from "react";
+import ItemCount from "../ItemCount/ItemCount";
+import "./ItemListContainer.css";
+import productos from "../../productos";
+import ItemList from "../ItemList/ItemList";
 
-const ItemListContainer = ({greeting}) =>{
-    function onAdd (count) {
-        console.log(`se agregado ${count} productos`)
-    }
+const ItemListContainer = ({ greeting }) => {
+  function onAdd(count) {
+    console.log(`se agregado ${count} productos`);
+  }
 
-    return (
-        <div className='itemListContainer'>
-           <h3 className='titleItemListContainer'>{greeting}</h3>
-            <ItemCount
-                stock={5}
-                initial={1}
-                onAdd={onAdd}
-            />
-        </div>
-    )
-}
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getProductos = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(productos);
+      }, 2000);
+    });
+
+    getProductos
+      .then((response) => setItems(response))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div className="itemListContainer">
+      <h3 className="titleItemListContainer">{greeting}</h3>
+      {loading ? <p>LOADING...</p> : <ItemList items={items} />}
+      <ItemCount stock={5} initial={1} onAdd={onAdd} />
+    </div>
+  );
+};
 
 export default ItemListContainer;
